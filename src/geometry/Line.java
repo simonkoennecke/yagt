1b4d3a5bb9acddcbcf9e4eb0c58884f6bbe27d94
Simon Könnecke <simonkoennecke@gmail.com>
2015-04-25 17:12:42 +0200

    add maven

--- src/geometry/Line.java
+++
@@ -1,9 +0,0 @@
-package geometry;
-
-public interface Line {
-    Point intersection(Line line);
-    Point getLocationVector();
-    Point getAnotherPointOnLine();
-    Point getNormalVector();
-    double getDistance(Point pt);
-}
