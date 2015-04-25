6426e29abd3944df6c6c904ca3f1ff3032114e29
Simon Könnecke <simonkoennecke@gmail.com>
2015-04-25 17:13:30 +0200

    add maven

---
+++ src/main/java/geometry/OrientatedLine2D.java
@@ -0,0 +1,96 @@
+package geometry;
+
+/**
+ * 2-Dimensional Line in Hesse normal form.
+ */
+public class OrientatedLine2D implements Line {
+    /**
+     * mP = location vector
+     * mQ = another vector on line
+     * mR = direction vector
+     * mN = normal vector
+     */
+    private Point mP, mQ, mR, mN;
+    private double mD;
+
+    /**
+     *
+     * @param p location vector
+     * @param r direction vector
+     */
+    public OrientatedLine2D(Point p, Point r){
+        mP = p;
+        mR = r;
+        mN = r.rotate90Degrees().normalize();
+        mD = p.dotProduct(mN);
+    }
+
+    public Point intersection(OrientatedLine2D line) {
+        // We need two points of each line.
+        Point p1 = getLocationVector();
+        Point p2 = getAnotherPointOnLine();
+        Point p3 = line.getLocationVector();
+        Point p4 = line.getAnotherPointOnLine();
+
+        //Please see for the formula here:
+        //http://en.wikipedia.org/wiki/Line%E2%80%93line_intersection#Given_two_points_on_each_line
+
+        //When this is zero the both lines are parallel.
+        if (((p1.getX() - p2.getX())*(p3.getY() - p4.getY())) - ((p1.getY() - p2.getY()) * (p3.getX() - p4.getX())) == 0) {
+            return null;
+        }
+
+        double x, y, z;
+        //Divider
+        z = ((p1.getX() - p2.getX()) * (p3.getY() - p4.getY())) -
+                ((p1.getY() - p2.getY()) * (p3.getX() - p4.getX()));
+
+        x = (((p1.getX() * p2.getY()) - (p1.getY() * p2.getX())) * (p3.getX() - p4.getX())) -
+                (p1.getX() - p2.getX()) * (p3.getX() * p4.getY() - p3.getY() * p4.getX());
+        x /= z;
+        y = (p1.getX() * p2.getY() - p1.getY() * p2.getX()) * (p3.getY() - p4.getY()) -
+                (p1.getY() - p2.getY()) * (p3.getX() * p4.getY() - p3.getY() * p4.getX());
+        y /= z;
+        return new Point2D(x, y);
+    }
+
+    @Override
+    public Point intersection(Line line) {
+        OrientatedLine2D l = new OrientatedLine2D(line.getLocationVector(),
+                line.getLocationVector().subtract(line.getAnotherPointOnLine()));
+        return intersection(l);
+    }
+
+    public Point getAnotherPointOnLine() {
+        if (mQ == null) {
+            mQ = mP.add(mR);
+        }
+        return mQ;
+    }
+
+
+    @Override
+    public Point getLocationVector() {
+        return mP;
+    }
+
+    @Override
+    public Point getNormalVector() {
+        return mN;
+    }
+
+    @Override
+    /**
+     * Get Distance of the Point to the line.
+     */
+    public double getDistance(Point pt) {
+        return (mN.dotProduct(pt) - mD);
+    }
+
+    public String toString() {
+        return "OrientatedLine2D(p: " + getLocationVector() +
+                ", q: " + getAnotherPointOnLine() +
+                ", r: " + mR +
+                ", n: " + getNormalVector() + ")";
+    }
+}
---
+++ src/main/java/geometry/Point2D.java
@@ -0,0 +1,97 @@
+package geometry;
+
+public class Point2D implements Point {
+    private double mX, mY;
+
+    Point2D(){
+
+    }
+    Point2D(Point pt){
+        setPoint(pt);
+    }
+    public Point2D(double x, double y){
+        setPoint(x, y);
+    }
+
+    @Override
+    public void setPoint(double x, double y) {
+        mX = x;
+        mY = y;
+    }
+
+    @Override
+    public void setPoint(Point pt) {
+        mX = pt.getX();
+        mY = pt.getY();
+    }
+
+    @Override
+    public double getX() {
+        return mX;
+    }
+
+    @Override
+    public double getY() {
+        return mY;
+    }
+
+    @Override
+    public Point normalize() {
+        double norm = norm();
+        mX /= norm;
+        mY /= norm;
+        return this;
+    }
+
+    @Override
+    public Point subtract(Point pt) {
+        return new Point2D(getX() - pt.getX(), getY() - pt.getY());
+    }
+
+    @Override
+    public Point add(Point pt) {
+        return new Point2D(getX() + pt.getX(), getY() + pt.getY());
+    }
+
+    @Override
+    public Point add(double val) {
+        mX += val;
+        mY += val;
+        return this;
+    }
+
+    @Override
+    public Point multiply(Point pt) {
+        return new Point2D(getX() * pt.getX(), getY() * pt.getY());
+    }
+
+    @Override
+    public Point multiply(double val) {
+        return new Point2D(getX() * val, getY() * val);
+    }
+
+    @Override
+    public Point rotate90Degrees() {
+        return new Point2D(getY(), -getX());
+    }
+
+    @Override
+    public double dotProduct(Point pt) {
+        return Math.sqrt(getX()*pt.getX()+getY()*pt.getY());
+    }
+
+    @Override
+    public double norm() {
+        return dotProduct(this);
+    }
+
+    @Override
+    public double distance(Point pt) {
+        return Math.sqrt(Math.pow(getX() - pt.getX(), 2) + Math.pow(getY() - pt.getY(), 2));
+    }
+
+    @Override
+    public String toString(){
+        return "Pt2D(x: " + getX() + ", y: " + getY()+ ")";
+    }
+}
