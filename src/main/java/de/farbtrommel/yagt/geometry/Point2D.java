d70f24db21a6d5b2583426355f4637243ef1a3e3
Simon Könnecke <simonkoennecke@gmail.com>
2015-04-30 13:19:31 +0200

    Add Homework 2.1

--- src/main/java/de/farbtrommel/yagt/geometry/Point2D.java
+++ src/main/java/de/farbtrommel/yagt/geometry/Point2D.java
@@ -13,29 +13,24 @@ public class Point2D implements Point {
         setPoint(x, y);
     }
 
-    @Override
     public void setPoint(double x, double y) {
         mX = x;
         mY = y;
     }
 
-    @Override
     public void setPoint(Point pt) {
         mX = pt.getX();
         mY = pt.getY();
     }
 
-    @Override
     public double getX() {
         return mX;
     }
 
-    @Override
     public double getY() {
         return mY;
     }
 
-    @Override
     public Point normalize() {
         double norm = norm();
         mX /= norm;
@@ -43,54 +38,56 @@ public class Point2D implements Point {
         return this;
     }
 
-    @Override
     public Point subtract(Point pt) {
         return new Point2D(getX() - pt.getX(), getY() - pt.getY());
     }
 
-    @Override
     public Point add(Point pt) {
         return new Point2D(getX() + pt.getX(), getY() + pt.getY());
     }
 
-    @Override
     public Point add(double val) {
         mX += val;
         mY += val;
         return this;
     }
 
-    @Override
     public Point multiply(Point pt) {
         return new Point2D(getX() * pt.getX(), getY() * pt.getY());
     }
 
-    @Override
     public Point multiply(double val) {
         return new Point2D(getX() * val, getY() * val);
     }
 
-    @Override
     public Point rotate90Degrees() {
         return new Point2D(getY(), -getX());
     }
 
-    @Override
+    public Point rotate(double degree) {
+        double rad = Math.toDegrees(degree);
+        return new Point2D(
+                getX() * Math.cos(rad) - getY() * Math.sin(rad),
+                getX() * Math.sin(rad) + getY() * Math.cos(rad)
+        );
+    }
+
+    public double angle(Point pt) {
+        return Math.toDegrees(Math.acos(dotProduct(pt) / (norm() * pt.norm())));
+    }
+
     public double dotProduct(Point pt) {
-        return Math.sqrt(getX()*pt.getX()+getY()*pt.getY());
+        return getX() * pt.getX() + getY() * pt.getY();
     }
 
-    @Override
     public double norm() {
-        return dotProduct(this);
+        return Math.sqrt(getX() * getX() + getY() * getY());
     }
 
-    @Override
     public double distance(Point pt) {
         return Math.sqrt(Math.pow(getX() - pt.getX(), 2) + Math.pow(getY() - pt.getY(), 2));
     }
 
-    @Override
     public String toString(){
         return "Pt2D(x: " + getX() + ", y: " + getY()+ ")";
     }
